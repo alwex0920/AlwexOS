7e4cc88e5534a66b809c293ff68607aace861473
alwex0920 <147579253+alwex0920@users.noreply.github.com>
2025-08-30 19:06:12 +0800

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,7 +1,9 @@
 #include "include/lib.h"
-#include "include/ata.h"
+#include "include/ahci.h"
 #include "include/fs.h"
 #include "include/bootinfo.h"
+
+extern uint32_t _end;
 
 void kmain(boot_info_t* boot_info) {
     init_framebuffer(boot_info);
@@ -11,8 +13,17 @@ void kmain(boot_info_t* boot_info) {
     }
     serial_init();
     clear_screen();
-    ata_init();
-    fs_init();    
+    uint32_t heap_start = (uint32_t)&_end;
+    uint32_t heap_size = 16 * 1024 * 1024;
+    mm_init(heap_start, heap_size);
+    ahci_init();
+    uint32_t fs_lba = find_fs_partition();
+    if (fs_lba == 0) {
+        print("Error: No filesystem partition found\n");
+        fs_init_ramdisk();
+    } else {
+        fs_init(fs_lba);  // Передаем LBA раздела
+    }    
     shell_main();
     while (1) {
         asm volatile ("hlt");
