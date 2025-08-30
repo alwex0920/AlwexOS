a65edfdb486d7873b0d612847ea84fa5cb303686
alwex0920 <147579253+alwex0920@users.noreply.github.com>
2025-08-30 19:12:46 +0800

    Create pci.c

---
+++ kernel/pci.c
@@ -0,0 +1,14 @@
+#include "include/pci.h"
+#include "include/lib.h"
+
+uint32_t pci_read_dword(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset) {
+    uint32_t address = (1 << 31) | (bus << 16) | (slot << 11) | (func << 8) | (offset & 0xFC);
+    outl(0xCF8, address);
+    return inl(0xCFC);
+}
+
+void pci_write_dword(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset, uint32_t value) {
+    uint32_t address = (1 << 31) | (bus << 16) | (slot << 11) | (func << 8) | (offset & 0xFC);
+    outl(0xCF8, address);
+    outl(0xCFC, value);
+}
