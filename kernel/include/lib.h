bb9bf270338e764a87ae46761d2b09d284219f19
alwex0920 <147579253+alwex0920@users.noreply.github.com>
2025-08-30 18:57:07 +0800

    Update lib.h

--- kernel/include/lib.h
+++ kernel/include/lib.h
@@ -30,6 +30,20 @@ void outb(uint16_t port, uint8_t val);
 uint8_t inb(uint16_t port);
 void outw(uint16_t port, uint16_t val);
 uint16_t inw(uint16_t port);
+static inline void outl(uint16_t port, uint32_t val) {
+    asm volatile ("outl %0, %1" : : "a"(val), "Nd"(port));
+}
+
+static inline uint32_t inl(uint16_t port) {
+    uint32_t ret;
+    asm volatile ("inl %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
+
+// Функции для работы с памятью
+static inline void io_wait(void) {
+    outb(0x80, 0);
+}
 void msleep(int milliseconds);
 void sleep(int seconds);
 
